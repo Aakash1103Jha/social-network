c9d5d3e8e8b2f485f7b27ef08df830e298d55cf1
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-27 00:04:03 +0530

    State management with redux
    - added a scaffold redux code for auth state
    - using `react-redux` and `redux-toolkit`
    - dummy actions for onSignin and onSignout
    - Testing conditional rendering based on `isLoggedIn` flag

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,16 @@ import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+
+import reduxAuthStore from "./redux/redux.js"
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={reduxAuthStore}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root"),
