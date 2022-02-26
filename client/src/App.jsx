a69e4964aff4538c34c35cfee16d5dae098083f1
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-26 20:53:02 +0530

    Lazy loading pages
    - lazy loading with React.Suspense and React.lazy
    - fallback component `Loader`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router"
 
 import styles from "./App.module.css"
+import Loader from "./components/Loader/Loader"
+
+const Homepage = lazy(() => import("./pages/Homepage/Homepage"))
 
 const App = () => {
-	return <div className={styles.App}>{/* <Button label="Click Me" /> */}</div>
+	return (
+		<div className={styles.App}>
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={
+						<Suspense fallback={<Loader />}>
+							<Homepage />
+						</Suspense>
+					}
+				/>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
---
+++ client/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./Homepage.module.css"
+
+const Homepage = () => {
+	return <div className={styles.homepage}></div>
+}
+
+export default Homepage
